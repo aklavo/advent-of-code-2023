c400655a8d8954c15c355f3828a3ce6155a72b07
andrew <aklavo@comcast.net>
2023-12-01 11:06:52 -0700

    working solution to day-1

--- Day-1/answer-day-1.py
+++ Day-1/answer-day-1.py
@@ -1,11 +1,32 @@
+# Open input file and read lines
 with open('input-day-1.txt') as f:
   lines = f.readlines()
-  print(lines[0])
-  answer = 0
-  for line in lines:
-    for char in lines:
-      if char.isdigit():
-        first_num = char
-        last_num = char
-        two_digit_num = int(first_num + last_num)
-    answer += two_digit_num
+
+# Initialize answer variable  
+answer = 0
+  
+# Loop through each line
+for line in lines:
+
+  # Initialize first digit
+  first_num = 0
+    
+  # Loop through characters in line
+  for char in line:
+
+    # If character is a digit and first isnot set, set first digit and last digit 
+    if char.isdigit() and first_num == 0:
+      first_num = char
+      last_num = char
+    # Continue to update last digit to current char if it is a digit
+    if char.isdigit():
+      last_num = char
+        
+  # Calculate calibration value from first and last digits    
+  calibration_value = int(first_num + last_num)
+  
+  # Add calibration value to running total
+  answer += calibration_value
+
+# Print final answer  
+print(answer)
