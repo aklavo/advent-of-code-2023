aba44428f6e65d45bebf48fcd2d2a55f458e89f9
andrew <aklavo@comcast.net>
2023-12-05 20:07:45 -0700

    Initialize day-5 part 1

---
+++ Day-5/answer-day-5.py
@@ -0,0 +1,10 @@
+# Open input file and read lines
+with open('input-day-4.txt') as f: #input-day-2
+    lines = f.readlines()
+    
+    lowest_location = 0
+    
+    for line in lines:
+        pass
+    
+print(lowest_location)
